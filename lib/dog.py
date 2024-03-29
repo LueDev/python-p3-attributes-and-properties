edd6c8dab5d0b8f367bb4772562178aab6597e3e
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-03-29 03:05:40 -0400

    All tests passing

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+from typing import Any
+import random
+
+
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +16,41 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Nimbus", breed=random.choice(APPROVED_BREEDS)):
+        ## Using the public property var as opposed to the internal _var for validations to occur during object initialization
+        self.name = name
+        self.breed = breed
+
+    def getName(self):
+        # print("retrieving name")
+        return self._name    
+    
+    def setName(self, name):
+        # print("setting name")
+        if type(name) == str and 1 <= len(name) <= 25:
+            self._name = name 
+        else: 
+            print("Name must be string between 1 and 25 characters.")
+    
+    name = property(getName, setName)
+    
+    def getBreed(self):
+        # print("retrieving name")
+        return self._breed    
+    
+    def setBreed(self, breed):
+        # print("setting name")
+        if type(breed) == str and breed in APPROVED_BREEDS:
+            self._breed = breed 
+        else: 
+            print("Breed must be in list of approved breeds.")
+    
+    breed = property(getBreed, setBreed)
+    
+    
+    def __repr__(self):
+        return f"{self.name} the Dog is a {self._breed}"
+
+fido = Dog("fido")
+fido.name = 66
+# print(type(fido) == Dog)
--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,42 @@
 #!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
+import random 
+approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
 
 class Person:
-    pass
+    
+    
+    def __init__(self, name="Rick", job=random.choice(approved_jobs)):
+        
+        # set the property as this will call our validation methods getName and setName to confirm the values we want are being used to create new instances
+        self.name = name
+        self.job = job
+
+    def getName(self):
+        # print("retrieving name")
+        return self._name    
+    
+    def setName(self, name):
+        # print("setting name")
+        if type(name) == str and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else: 
+            print("Name must be string between 1 and 25 characters.")
+    
+    name = property(getName, setName)
+    
+    def getJob(self):
+        return self._job    
+    
+    def setJob(self, job):
+        if job in approved_jobs:
+            self._job = job
+        else: 
+            print("Job must be in list of approved jobs.")
+    
+    job = property(getJob, setJob)
+    
+    def __repr__(self):
+        return f"{self.name} the Person works in {self.job}"
+    
+phil = Person()
+print(phil)
